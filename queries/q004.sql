0f918dcaaf8c3c3f0d90d6d7a33be72c2384126e
Nikolaos Kormpakis <el10785@mail.ntua.gr>
2026-02-06 22:42:48 +0200

    fix(queries): Add correct fields for q004

--- queries/q004.sql
+++ queries/q004.sql
@@ -1,15 +1,23 @@
-SELECT ss_customer_sk ,sum(act_sales) sumsales
-FROM (SELECT ss_item_sk
-            ,ss_ticket_number
-            ,ss_customer_sk
-            ,CASE WHEN sr_return_quantity IS NOT NULL
-                THEN (ss_quantity-sr_return_quantity)*ss_sales_price
-                ELSE (ss_quantity*ss_sales_price)
-            END act_sales
-      FROM store_sales LEFT OUTER JOIN store_returns
-      ON (sr_item_sk = ss_item_sk AND sr_ticket_number = ss_ticket_number) ,reason
-            WHERE sr_reason_sk = r_reason_sk
-              AND r_reason_desc = 'reason 49') t
-      GROUP BY ss_customer_sk
-      ORDER BY sumsales, ss_customer_sk
-limit 100;
+SELECT
+  ss_customer_sk,
+  sum(act_sales) AS sumsales
+FROM (
+  SELECT
+    ss.ss_item_sk,
+    ss.ss_ticket_number,
+    ss.ss_customer_sk,
+    CASE
+      WHEN sr.sr_return_quantity IS NOT NULL
+        THEN (ss.ss_quantity - sr.sr_return_quantity) * ss.ss_sales_price
+      ELSE ss.ss_quantity * ss.ss_sales_price
+    END AS act_sales
+  FROM psql.tpcds.store_sales ss
+  LEFT JOIN psql.tpcds.store_returns sr
+    ON sr.sr_item_sk = ss.ss_item_sk
+   AND sr.sr_ticket_number = ss.ss_ticket_number
+  JOIN psql.tpcds.reason r
+    ON sr.sr_reason_sk = r.r_reason_sk
+) t
+GROUP BY ss_customer_sk
+ORDER BY sumsales, ss_customer_sk
+LIMIT 100;
